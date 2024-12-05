a091b3274baff70c92415f3450cdd52d6a481bfd
Jewpeppi <456jljes@gmail.com>
2024-12-05 20:52:31 +0000

    Fast & Furious 6

--- Assignment2-Ordered-Set/OrderedList.h
+++ Assignment2-Ordered-Set/OrderedList.h
@@ -16,10 +16,10 @@
  * - The `orderedIntSet` structure maintains the size of the set and a pointer to the double-linked list storing the elements.
  * - Return values for operations are represented by the `ReturnValues` enumeration.
  *
- * @author
- * @note Coding: Rory Huynh - 23374624
- * @author
- * @note Documentation: Joseph Ennis - 23360798
+ * @author Rory Huynh - 23374624
+ * @note Coding
+ * @author Joseph Ennis - 23360798
+ * @note Documentation
  * @date 04/12/2024
  *
  * @note Additional group members include: Ali Farah (23380098), Sean Kirk (23376201), Mihail Bizjajevs (23364734)
@@ -49,71 +49,13 @@ typedef enum {
     NUMBER_REMOVED, /* 3 */ /**< Element was successfully removed from the set. */
     ALLOCATION_ERROR /* 4 */ /**< Memory allocation error occurred. */
 } ReturnValues;
-/**
- * @brief Creates a new, empty ordered set.
- *
- * @return Pointer to the newly created ordered set.
- */
+
 orderedIntSet* createOrderedSet();
-
-/**
- * @brief Deletes an ordered set and frees its memory.
- *
- * @param s Pointer to the ordered set to be deleted.
- * @return ReturnValues indicating success or failure.
- */
 ReturnValues deleteOrderedSet(orderedIntSet* s);
-
-/**
- * @brief Adds an integer to the ordered set, maintaining sorted order.
- *
- * @param s Pointer to the ordered set.
- * @param elem The integer element to add.
- * @return ReturnValues indicating the result of the operation.
- */
 ReturnValues addElement(orderedIntSet* s, int elem);
-
-/**
- * @brief Removes an integer from the ordered set.
- *
- * @param s Pointer to the ordered set.
- * @param elem The integer element to remove.
- * @return ReturnValues indicating the result of the operation.
- */
 ReturnValues removeElement(orderedIntSet* s, int elem);
-
-/**
- * @brief Computes the intersection of two ordered sets.
- *
- * @param s1 Pointer to the first ordered set.
- * @param s2 Pointer to the second ordered set.
- * @return Pointer to a new ordered set containing the intersection.
- */
 orderedIntSet* setIntersection(orderedIntSet* s1, orderedIntSet* s2);
-
-/**
- * @brief Computes the intersection of two ordered sets.
- *
- * @param s1 Pointer to the first ordered set.
- * @param s2 Pointer to the second ordered set.
- * @return Pointer to a new ordered set containing the intersection.
- */
 orderedIntSet* setUnion(orderedIntSet* s1, orderedIntSet* s2);
-
-/**
- * @brief Computes the difference of two ordered sets (s1 - s2).
- *
- * @param s1 Pointer to the first ordered set.
- * @param s2 Pointer to the second ordered set.
- * @return Pointer to a new ordered set containing the difference.
- */
 orderedIntSet* setDifference(orderedIntSet* s1, orderedIntSet* s2);
-
-/**
- * @brief Prints the contents of the ordered set to the standard output.
- *
- * @param s Pointer to the ordered set to print.
- * @return Always returns 0.
- */
 int printToStdout(orderedIntSet* s);
 #endif
