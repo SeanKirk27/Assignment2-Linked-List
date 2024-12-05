5c52d5837056e3930074682b04533352d3eb64db
Random-Github-Wanderer <52551032+Random-Github-Wanderer@users.noreply.github.com>
2024-12-05 01:58:59 +0000

    Update OrderedList.h
    Updated the ordered set header file so that it excludes any structs that have already been made in Sean's code. It also has an updated orderedIntSet struct that uses a double linked list as it's data.

--- Assignment2-Ordered-Set/OrderedList.h
+++ Assignment2-Ordered-Set/OrderedList.h
@@ -1,24 +1,10 @@
 #ifndef OrderedList_h
 #define OrderedList_h
 
-typedef struct Node { /* Rory Huynh: I am just copying Sean's node structure to keep the code consistent. */
-    int data;
-    struct Node* prev;
-    struct Node* next;
-} Node;
-
-typedef struct orderedIntSet { /* Rory Huynh: Conventionally, structures should be defined with an upper camel case naming scheme but he specifically wanted it to be this so we will go with that :| */
+typedef struct orderedIntSet {
     int size;
-    Node* head;
+    DoubleLinkedList *list;
 } orderedIntSet;
-
-typedef enum {
-    NUMBER_ALREADY_IN_SET, /* 0 */
-    NUMBER_NOT_IN_SET, /* 1 */
-    NUMBER_ADDED, /* 2 */
-    NUMBER_REMOVED, /* 3 */
-    ALLOCATION_ERROR /* 4 */
-} ReturnValues;
 
 orderedIntSet* createOrderedSet();
 ReturnValues deleteOrderedSet(orderedIntSet* s);
